49b43d4f4ef6b43c6f13dab07f5cf66663e92e23
SowMansour <mansoursow10@gmail.com>
2023-08-06 17:15:55 +0200

    Step 2 : Throw 1 dice

--- js/app.js
+++ js/app.js
@@ -6,3 +6,18 @@ nodeDice.classList.add("dice");
 let divPlayer = document.getElementById("player");
 // Rajout de la div crée dans la page
 divPlayer.appendChild(nodeDice);
+
+/**
+ * Function qui génère un nombre aléatoire
+ * @returns {number}
+ */
+const randomDice = function() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+// On stocke le nbre aléatoire 
+const rand = randomDice();
+console.log(rand);
+
+// On modifie la position du face background selon son numéro (-000px, -100px ...)
+nodeDice.style.backgroundPositionX = `-${rand-1}00px`;
