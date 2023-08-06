820aadb5b58ef83c8e8ccacec75f8302461eac80
SowMansour <mansoursow10@gmail.com>
2023-08-06 17:38:46 +0200

    Step 2 : Throw several dices

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+/**
+ * Function qui génère un nombre aléatoire
+ * @returns {number}
+ */
+const randomDice = function() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+//Je crée une function contenant les instructions pour lancer un dé
+const throwDice = function(){
 // Création d'une div
 let nodeDice = document.createElement("div");
 
@@ -7,17 +16,20 @@ let divPlayer = document.getElementById("player");
 // Rajout de la div crée dans la page
 divPlayer.appendChild(nodeDice);
 
-/**
- * Function qui génère un nombre aléatoire
- * @returns {number}
- */
-const randomDice = function() {
-    return Math.floor(Math.random() * 6 + 1);
-}
-
 // On stocke le nbre aléatoire 
 const rand = randomDice();
-console.log(rand);
+//console.log(rand);
 
 // On modifie la position du face background selon son numéro (-000px, -100px ...)
 nodeDice.style.backgroundPositionX = `-${rand-1}00px`;
+
+};
+
+//Demander le nombre de dé ue l'utilisateur souhaite lancer
+const numberDice = Number(prompt("Combien de dé souhaites-tu que je lance"));
+
+//J'utilise une boucle pour éxecuter la function autant de fois que de dé demander
+for (let i = 0; i < numberDice; i++) {
+    throwDice();
+    
+}
